658f701b39f4d9a8f60c175d3fea9346c63ed66c
juliamalec <jmalec@uwo.ca>
2025-05-09 14:00:07 -0400

    updated read me

--- full_test_example/job.sh
+++ full_test_example/job.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=1          #Number of CPU cores per task
 #SBATCH --gres=gpu:1               #Number of GPUs per task 
 #SBATCH --mem=10G                  #Memory per nodes
-#SBATCH --time=24:00:00            #Time limit hrs:min:sec
+#SBATCH --time=1:30:00            #Time limit hrs:min:sec
 
 
 srun ./testing_code.py cd01068
